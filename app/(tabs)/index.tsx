3b9c2cecd6b8bc7453ea4752a9fa1735b91d39b0
ruxinic <ruxinicu@gmail.com>
2026-01-07 10:16:05 +0200

    initialized buttons with modals

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,5 +1,7 @@
 import { Text, View, StyleSheet} from 'react-native';
- import { Image } from 'expo-image'; 
+import { Image } from 'expo-image'; 
+import ImageViewer from "@/components/ImageViewer";
+import Button from "@/components/Button";
 
 
 const ruxiImage = require('@/assets/images/Subject.png');
@@ -9,8 +11,12 @@ export default function Index() {
   return (
     <View style={styles.container}>
       <View style={styles.imageContainer}>
-        <Image source={ruxiImage} style={styles.image} />
+        <ImageViewer source={ruxiImage} />
         <Text>this is the home page! find something about me! there will be modals with fun facts</Text>
+      </View>
+      <View style={styles.footerCont}>
+        <Button theme="primary" label="Find Out!"/>
+        <Button label="Diff button!"/>
       </View>
     </View>
   );
@@ -29,5 +35,9 @@ const styles = StyleSheet.create({
     width: 420,
     height: 310,
     borderRadius: 15,
+  },
+  footerCont: {
+    flex: 1 /3,
+    alignItems: 'center',
   },
 });
